a372d42689d0855f2e166a7c7aeed01f783f4d06
Bezrukov Yakov <sv.nnov@gmail.com>
2021-07-22 18:04:31 +0300

    Counter Button task solved

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,15 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import {createApp} from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+createApp({
+  data() {
+    return {
+      counter: 0
+    };
+  },
+  methods: {
+    plus() {
+      this.counter++;
+    }
+  },
+  template: `<button type="button" v-on:click="plus">{{ counter }}</button>`
+}).mount('#app');
