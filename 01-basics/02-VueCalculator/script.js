ba97ad9c9d253d6b73aa353eabed924f065018df
Bezrukov Yakov <sv.nnov@gmail.com>
2021-07-22 18:32:12 +0300

    Calculator task solved

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,31 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import {createApp} from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+createApp({
+  data() {
+    return {
+      firstNum: 0,
+      secondNum: 0,
+      action: 'sum'
+    };
+  },
+  computed: {
+    result() {
+      switch (this.action) {
+        case 'sum':
+          return this.firstNum + this.secondNum;
+          break;
+        case 'subtract':
+          return this.firstNum - this.secondNum;
+          break;
+        case 'multiply':
+          return this.firstNum * this.secondNum;
+          break;
+        case 'divide':
+          return this.firstNum / this.secondNum;
+          break;
+        default:
+          throw new Error('Unexpected action');
+      }
+    }
+  },
+}).mount('#app');
